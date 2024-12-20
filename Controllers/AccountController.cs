8dbe0caa879008937846f780691ddff78ef4d846
Danijel Bogicevic <dan.bogicevic@gmail.com>
2024-12-20 02:00:41 +0100

    -Fix return Register and Login

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -6,7 +6,7 @@ namespace WorkshopManager.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class AccountController : Controller
+    public class AccountController : ControllerBase
     {
         private readonly IAccountService _accountService;
 
@@ -15,14 +15,15 @@ namespace WorkshopManager.Controllers
             _accountService = accountService;
         }
 
-        [HttpPost("register")]
+        [HttpPost]
         public async Task<IActionResult> Register([FromBody] RegisterDTO registerDTO)
         {
             if (!ModelState.IsValid)
                 return BadRequest(ModelState);
 
             var result = await _accountService.RegisterAsync(registerDTO);
-            if (result.Succeeded) return Ok("User registered successfully");
+            if (result.Succeeded)
+                return CreatedAtAction(nameof(Register), new { email = registerDTO.Email }, registerDTO);
 
             return BadRequest(result.Errors);
         }
@@ -35,10 +36,10 @@ namespace WorkshopManager.Controllers
 
             var result = await _accountService.LoginAsync(loginDTO);
 
-            if (result.Succeeded) return Ok("Login successful.");
+            if (result.Succeeded)
+                return Ok(new { message = "Login successful." });
 
             return Unauthorized("Invalid login attempt.");
         }
-
     }
 }
