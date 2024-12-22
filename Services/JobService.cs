d6d2e5472616fddba0667664a91409bd878f80bc
Danijel Bogicevic <dan.bogicevic@gmail.com>
2024-12-22 15:12:15 +0100

    - Refactor JobService

--- Services/JobService.cs
+++ Services/JobService.cs
@@ -6,16 +6,20 @@ using WorkshopManager.Interfaces.ServiceInterfaces;
 
 namespace WorkshopManager.Services
 {
-    public class JobService(IUnitOfWork unitOfWork, IMapper mapper) : IJobService
+    public class JobService : IJobService
     {
-        private readonly IUnitOfWork _unitOfWork = unitOfWork;
-        private readonly IMapper _mapper = mapper;
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+
+        public JobService(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
 
         public async Task<JobDTO> CreateJobAsync(RequestCreateJobDTO createJob)
         {
-            var worker = await _unitOfWork.WorkerRepository.GetWorkerByIdAsync(createJob.WorkerId)
-                ?? throw new WorkerNotFoundException(createJob.WorkerId);
-            var workerFullName = worker.FullName;
+            var workerFullName = await GetWorkerFullNameAsync(createJob.WorkerId);
 
             var jobCreate = _mapper.Map<JobDTO>(createJob);
             jobCreate.WorkerName = workerFullName;
@@ -28,9 +32,7 @@ namespace WorkshopManager.Services
 
         public async Task<JobDTO> UpdateJobAsync(int id, RequestUpdateJobDTO updateJob)
         {
-            var worker = await _unitOfWork.WorkerRepository.GetWorkerByIdAsync(updateJob.WorkerId)
-                ?? throw new WorkerNotFoundException(updateJob.WorkerId);
-            var workerFullName = worker.FullName;
+            var workerFullName = await GetWorkerFullNameAsync(updateJob.WorkerId);
 
             var jobUpdate = _mapper.Map<JobDTO>(updateJob);
             jobUpdate.WorkerName = workerFullName;
@@ -46,7 +48,7 @@ namespace WorkshopManager.Services
             var job = await _unitOfWork.JobRepository.GetJobByIdAsync(id)
                 ?? throw new JobNotFoundException(id);
 
-            var isDeleted = _unitOfWork.JobRepository.DeleteJob(job);
+            bool isDeleted = _unitOfWork.JobRepository.DeleteJob(job);
             if (isDeleted)
             {
                 await _unitOfWork.SaveChangesAsync();
@@ -64,6 +66,13 @@ namespace WorkshopManager.Services
             var getJob = _mapper.Map<JobDTO>(job);
 
             return getJob;
+        }
+
+        private async Task<string> GetWorkerFullNameAsync(int workerId)
+        {
+            var worker = await _unitOfWork.WorkerRepository.GetWorkerByIdAsync(workerId)
+                ?? throw new WorkerNotFoundException(workerId);
+            return worker.FullName;
         }
     }
 }
