e7c81190d4e2bc5d779ebc0d7c5ec73e3c2014a7
Danijel Bogicevic <dan.bogicevic@gmail.com>
2025-02-04 18:11:59 +0100

    - margin top for heading

--- workshop-manager-frontend/src/components/atoms/StatCard.js
+++ workshop-manager-frontend/src/components/atoms/StatCard.js
@@ -3,7 +3,7 @@ import React from "react";
 const StatCard = ({ label, value, isLoading, error, icon: Icon, colorClass }) => {
     return (
         <div className="flex flex-col items-center">
-            <h3 className="text-lg font-semibold text-white flex items-center gap-2">
+            <h3 className="text-lg font-semibold text-white flex items-center mt-3 gap-2">
                 <Icon className={`${colorClass} text-xl`} />
                 {label}
             </h3>
