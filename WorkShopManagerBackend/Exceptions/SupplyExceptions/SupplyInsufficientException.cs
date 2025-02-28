aa66fae5dbd8384c9f7cd074a785553c59972e62
Danijel Bogicevic <dan.bogicevic@gmail.com>
2025-02-28 16:10:30 +0100

    - custom exceptions for inssuficient supply quantity

---
+++ WorkShopManagerBackend/Exceptions/SupplyExceptions/SupplyInsufficientException.cs
@@ -0,0 +1,8 @@
+﻿namespace WorkshopManager.Exceptions.SupplyExceptions
+{
+    public class SupplyInsufficientException : Exception
+    {
+        public SupplyInsufficientException(int supplyId)
+            : base($"Supply with ID {supplyId} does not have sufficient quantity.") { }
+    }
+}
