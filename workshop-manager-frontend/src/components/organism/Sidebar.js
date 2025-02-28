81085b23e1cefbf1ca422e58f0d4de3e8b3c7793
Danijel Bogicevic <dan.bogicevic@gmail.com>
2025-02-28 23:45:25 +0100

    - added padding

--- workshop-manager-frontend/src/components/organism/Sidebar.js
+++ workshop-manager-frontend/src/components/organism/Sidebar.js
@@ -74,7 +74,7 @@ const Sidebar = () => {
                     </button>
                 </div>
 
-                <div>
+                <div className='p-2'>
                     <Outlet />
                 </div>
             </div>
